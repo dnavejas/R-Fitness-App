a8c3003836941280eb335cd60b29ecc48e8f3e61
Danny Navejas <dannynavejas@gmail.com>
2019-07-21 13:16:59 -0500

    re-initialized react app, changed dropdown menu items to match those in trello board

--- ro-fitness-app/src/App.js
+++ ro-fitness-app/src/App.js
@@ -1,26 +1,39 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Fragment } from "react";
+import axios from "axios";
+import { Container, Row, Col } from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import Post from "./components/Workout-post";
+import Header from "./components/Header";
+
+const App = () => (
+  <Fragment>
+    <Header />
+
+    <main className="my-5 py-5">
+      <Container className="px-0">
+        <Row
+          noGutters
+          className="pt-2 pt-md-5 w-100 px-4 px-xl-0 position-relative"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          <Col
+            xs={{ order: 2 }}
+            md={{ size: 4, order: 1 }}
+            tag="aside"
+            className="pb-5 mb-5 pb-md-0 mb-md-0 mx-auto mx-md-0"
+          />
+
+          <Col
+            xs={{ order: 1 }}
+            md={{ size: 7, offset: 1 }}
+            tag="section"
+            className="py-5 mb-5 py-md-0 mb-md-0"
+          >
+            <Post />
+          </Col>
+        </Row>
+      </Container>
+    </main>
+  </Fragment>
+);
 
 export default App;
