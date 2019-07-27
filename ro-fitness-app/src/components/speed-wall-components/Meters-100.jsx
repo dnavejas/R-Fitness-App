2fe77ac8ccb51ca33dfb5ce58d3020a9496e3dea
Danny Navejas <dannynavejas@gmail.com>
2019-07-26 19:32:31 -0500

    Added speedwall components folder, added components for each distance, added those components in the speedwall file to display in each tab when that tab is selected

---
+++ ro-fitness-app/src/components/speed-wall-components/Meters-100.jsx
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+
+class Meters100 extends Component {
+  state = {};
+  render() {
+    return (
+      <Fragment>
+        <div>text here</div>
+      </Fragment>
+    );
+  }
+}
+
+export default Meters100;
